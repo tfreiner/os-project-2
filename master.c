3d85b8d3fc9d8d8aeea77af094bc98fa649539fb
tfreiner <tfreiner@gmail.com>
2017-09-25 23:59:52 -0500

    code cleanup

--- master.c
+++ master.c
@@ -1,7 +1,7 @@
 /**
  * Author: Taylor Freiner
  * Date: September 25, 2017
- * Log: More work on critical section 
+ * Log: Code cleanup 
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,16 +21,16 @@ int processcount = -1;
 int sharedmem[100];
 int processids[100];
 
-void clean(){
-	printf("Interrupt Triggered\n");
+void clean(int sig){
+	if(sig == 2)
+		fprintf(stderr, "Interrupt signaled. Removing shared memory and killing processes.\n");
+	else if(sig == 14)
+		fprintf(stderr, "Program timed out. Removing shared memory and killing processes.\n");	
 	int i;	
-	printf("Mem Count: %d\n", memcount);
 	for(i = 0; i < 3; i++){
-		printf("Exit Id: %d\n", sharedmem[memcount]);
 		shmctl(sharedmem[memcount], IPC_RMID, NULL);
 	}
 	for(i = -1; i < processcount; i++){
-		printf("Killing Process: %d\n", processids[processcount]);
 		kill(processids[processcount], SIGKILL);		
 	}
 }
@@ -42,7 +42,6 @@ int main(int argc, char* argv[]){
 	int count = 0;
 	char line[256];
 	pid_t childpid = 0;
-
 	//SIGNAL HANDLING
 	signal(SIGINT, clean);
 	signal(SIGALRM, clean);
@@ -55,6 +54,7 @@ int main(int argc, char* argv[]){
 		perror ("Error: ");
 		return 1;
 	}
+
 	size = sizeof(file);
 	
     	while (fgets(line, sizeof(line), file) != NULL)
@@ -89,7 +89,6 @@ int main(int argc, char* argv[]){
 				argval = *optarg;
 				if(isdigit(argval) && (atoi(optarg) > 0)){
 					max_time = atoi(optarg);
-					printf("Max Time: %d\n", max_time);
 					alarm(max_time);
 				}
 				else{
@@ -97,26 +96,21 @@ int main(int argc, char* argv[]){
 				}
 				break;
 			case '?':
-				fprintf(stderr, "%s Error: ADD USAGE STATEMENT %s\n", argv[0], argv[0]);
+				fprintf(stderr, "%s Error: Usage: %s <-t positive integer>\n", argv[0], argv[0]);
 				return 1;
 				break;
 		}
 	}
-	
 	//SHARED MEMORY
 	key_t key = ftok("keygen", 1);
 	key_t key2 = ftok("keygen2", 1);
 	key_t key3 = ftok("keygen3", 1);
-	//key_t key4 = ftok("keygen4", 1);
 	int memid = shmget(key, count*256, IPC_CREAT | 0644);
 	int memid2 = shmget(key2, sizeof(int*), IPC_CREAT | 0644);
 	int memid3 = shmget(key3, sizeof(int*)*19, IPC_CREAT | 0644);
-	//int memid4 = shmget(key4, sizeof(int*), IPC_CREAT | 0644);
-	printf("Initial ID: %d\n", memid);
 	sharedmem[0] = memid;
 	sharedmem[1] = memid2;
 	sharedmem[2] = memid3;
-	//sharedmem[3] = memid4;
 	if(memid == -1){
 		printf("%s: ", argv[0]);
 		perror("Error:");
@@ -125,80 +119,40 @@ int main(int argc, char* argv[]){
 	char (*mempoint)[256] = shmat (memid, NULL, 0);
 	int *mempoint2 = (int *)shmat (memid2, NULL, 0);
 	int *mempoint3 = (int *)shmat (memid3, NULL, 0);
-	//int *mempoint4 = (int *)shmat (memid4, NULL, 0);
 	if(*mempoint == -1)
-		printf("ERROR\n");
+		perror("Error\n");
 	if(*mempoint2 == -1)
-		printf("ERROR\n");
+		perror("Error\n");
 	if(*mempoint3 == -1)
-		printf("ERROR\n");
-	//if(*mempoint4 == -1)
-	//	printf("ERROR\n");
+		perror("Error\n");
 	for(i = 0; i < count; i++){
 		memcpy(mempoint[i], mylist[i], 256);
-		printf(".%s. ", mempoint[i]);
 	}
 	for(i = 0; i < 20; i++)
 		memcpy(&mempoint3[i], &i, 4);
 	int turnint = 0;
 	memcpy(mempoint2, &turnint, sizeof(int*));
-	printf("\n");
 
 	//PROCESSES
 	int processcount = 0;
 	int stringindex = 0;
 	int stringCount = count;
-	//*mempoint4 = 0;
-	//printf("INITIAIL STRING INDEX: %d\n", *mempoint4);
 	i = 0;
 	while(count > 0){
-		printf("PROCESS COUNT: %d\n", processcount);
 		if(processcount < 19){
 			if(count < 5)
 				stringindex = count % 5;
 			childpid = fork();
 			processcount++;
 			if(childpid > 0){
-				printf("Parent Process\n");
-				printf("%d\n", childpid);
 				processids[processcount] = childpid;
 			}
 			else if(childpid == 0){
-				printf("Child Process\n");
-
-				//*mempoint4 += 5;
-				//int stringindex = *mempoint4 - 5;
-				char arg[12], arg2[12], arg3[12], arg4[12];
+				char arg[12], arg2[12], arg3[12];
 				sprintf(arg, "%d", i);
 				sprintf(arg2, "%d", stringCount);
 				sprintf(arg3, "%d", stringindex);
-				//sprintf(arg4, "%d", processnum);
-				//arg = count % 5;
 				execl("palin", "palin", arg, arg2, arg3, NULL);
-		/*		if((stringindex + 5) <= count){
-					stringindex += 5;
-					execl("palin", "palin", arg, arg2, stringindex - 5, NULL);
-				}
-				else if((stringindex + 4) <= count){
-					stringindex += 4;
-					execl("palin", "palin", arg, arg2, stringindex - 4, NULL);
-				}
-				else if((stringindex + 3) <= count){
-					stringindex += 3;
-					execl("palin", "palin", arg, arg2, stringindex - 3, NULL);
-				}
-				else if((stringindex + 2) <= count){
-					stringindex += 2;
-					execl("palin", "palin", arg, arg2, stringindex - 2, NULL);
-				}
-				else if((stringindex + 1) <= count){
-					stringindex +=1;
-					execl("palin", "palin", arg, arg2, stringindex - 1, NULL);
-				}
-				else
-					execl("palin", "palin", arg, arg2, stringindex, NULL);
-		*/
-				break;
 			}
 			else if(childpid == -1){
 				printf("%s: ", argv[0]);
@@ -207,17 +161,14 @@ int main(int argc, char* argv[]){
 		}
 		else if(wait(NULL)){
 			processcount--;
-			printf("DECREMENT COUNTER\n");
 		}
 		count -= 5;
 		i++;
 	}
 	
-	printf("Out of loop\n");
 	sleep(40);
 	shmctl(memid, IPC_RMID, NULL);
 	shmctl(memid2, IPC_RMID, NULL);
 	shmctl(memid3, IPC_RMID, NULL);
-	//shmctl(memid4, IPC_RMID, NULL);
 	return 0;
 }
--- palin.c
+++ palin.c
@@ -1,7 +1,7 @@
 /**
  * Author: Taylor Freiner
  * Date: September 25, 2017
- * Log: More work on critical section
+ * Log: Code cleanup
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,39 +15,18 @@ void process(const int, int, int);
 void palin(int, int, int);
 
 int main(int argc, char* argv[]){	
-	printf("----------------------IN MAIN PALIN FILE-------------------------------\n");
-	int i, processIndex, stringCount, stringIndex;
+	int processIndex, stringCount, stringIndex;
 	processIndex = atoi(argv[1]);
-	printf("PROCESS INDEX: %d\n", processIndex);
 	stringCount = atoi(argv[2]);
-	//printf("STRING COUNT: %d\n", stringCount);
 	stringIndex = atoi(argv[3]);
 	if(stringIndex == 0)
 		stringIndex = 5;
-//	palin(stringCount, stringIndex);
-//	process(processIndex, stringIndex, stringCount);
 
-/*
-	for(i = 0; i < stringIndex; i++){
-		printf("-----------------------FOR LOOP-------------------------\n%d", processIndex);
-		srand(time(NULL));
-		float time1 = (float)rand()/(float)(RAND_MAX/2);
-		float time2 = (float)rand()/(float)(RAND_MAX/2);
-		sleep(time1);
-		//palin(stringCount, processIndex, (processIndex*5) + i);
-		process(processIndex, (processIndex*5) + i, stringCount);
-		printf("LEAVING CRITICAL SECTION\n");
-		sleep(time2);
-	}
-*/
 	process(processIndex, stringIndex, stringCount);
 	return 0;
 }
 
-void process(const int i, int stringindex, int count){
-	printf("------------------IN PROCESS-------------------------\n");
-	printf("\n\n\nPROCESS INDEX: %d\n\n\n", i);
-	printf("\n\n\nSTRING INDEX: %d\n\n\n", stringindex);	
+void process(const int i, int stringIndex, int stringCount){
 	key_t key2 = ftok("keygen2", 1);
 	key_t key3 = ftok("keygen3", 1);
 	int memid2 = shmget(key2, sizeof(int*), 0);
@@ -58,7 +37,7 @@ void process(const int i, int stringindex, int count){
 	int k;
 	for(k = 0; k < 20; k++)
 		 memcpy(&flag[k], &flagvar, 4);
-	int n = count;
+	int n = stringCount;
 	int j;
 	int idle = 0;
 	int want_in = 1;
@@ -66,74 +45,61 @@ void process(const int i, int stringindex, int count){
 	int exit = 0;
 	do {
 		do {
-			memcpy(&flag[i], &want_in, 4); // Raise my flag
-			j = *turn; // Set local variable
-			// wait until its my turn
+			memcpy(&flag[i], &want_in, 4);
+			j = *turn;
 			while ( j != i )
 				j = ( flag[j] != idle ) ? *turn : ( j + 1 ) % n;
-			// Declare intention to enter critical section
 			memcpy(&flag[i], &in_cs, 4);
-			// Check that no one else is in critical section
 			for ( j = 0; j < n; j++ )
 				if ( ( j != i ) && ( flag[j] == 2 ) )
 					break;
 		} while ( ( j < n ) || ( *turn != i && flag[*turn] != 0) );
-		// Assign turn to self and enter critical section
 		*turn = i;
-		for(k = 0; k < stringindex; k++){
-			printf("-----------------------FOR LOOP-------------------------\n%d", i);
+		for(k = 0; k < stringIndex; k++){
 			srand(time(NULL));
 			float time1 = (float)rand()/(float)(RAND_MAX/2);
 			float time2 = (float)rand()/(float)(RAND_MAX/2);
 			sleep(time1);
-			//palin(count, processIndex, (processIndex*5) + i);
-			palin(count, i, (i*5)+k);
+			palin(stringCount, i, (i*5)+k);
 			sleep(time2);
 		}
-		//palin(count, i, stringindex);
 		exit = 1;
 		j = (*turn + 1) % n;
 		while (flag[j] == 0)
 			j = (j + 1) % n;
-		// Assign turn to next waiting process; change own flag to idle
 		*turn = j; memcpy(&flag[i], &idle, 4);
 	
 	} while (exit == 0);
 }
 
-void palin(int count, int processIndex, int stringIndex){
-	printf("------------------IN PALIN---------------------------\n");
+void palin(int stringCount, int processIndex, int stringIndex){
 	int i = 0;
 	int j = 0;
-	//printf("COUNT: %d\n\n", count);
-	//printf("INDEX: %d\n\n", processIndex);
-	//printf("STRING INDEX: %d\n\n", stringIndex);
-//	char charindex[12];
-//	sprintf(charindex, "%d", index);	
 	FILE *palin_file = fopen("palin.out", "a");
 	FILE *non_palin_file = fopen("nopalin.out", "a");
 	key_t key = ftok("keygen", 1);
-	int memid = shmget(key, count*256, 0);
+	int memid = shmget(key, stringCount*256, 0);
 	char (*mystring)[256] = shmat (memid, NULL, 0);
-	for(i = 0; i < count; i++)
+	for(i = 0; i < stringCount; i++)
 		printf(".%s. ", mystring[i]);
-	printf("\n");
 	char* string = mystring[stringIndex];
 	j = strlen(string) - 1;
 	i = 0;
 	
 	while (i < j){
 		if (string[i] != string[j]){
-			fprintf(non_palin_file, string);
-			fprintf(non_palin_file, "\n");
+			fprintf(non_palin_file, "%ld ", (long)getpid());
+			fprintf(non_palin_file, "%d ", stringIndex);
+			fprintf(non_palin_file, "%s\n", string);
 			fclose(non_palin_file);
 			return;
 		}
 		i++;
 		j--;
 	}
-	fprintf(palin_file, string);
-	fprintf(palin_file, "\n");
+	fprintf(palin_file, "%ld ", (long)getpid());
+	fprintf(palin_file, "%d ", stringIndex);
+	fprintf(palin_file, "%s\n", string);
 	fclose(palin_file);
 	return;
 }
